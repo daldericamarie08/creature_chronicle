c132eaf8ba94595dfb441b655a9f748384cffa24
ricamariedalde <ricadalde0808@gmail.com>
2024-01-23 18:34:54 +0800

    Innitial commit

---
+++ explore.js
@@ -0,0 +1,47 @@
+let slideIndex = 1;
+
+// Function to show slides
+function showSlides(n) {
+let i;
+const slides = document.getElementsByClassName("explore-mySlides");
+const dots = document.getElementsByClassName("explore-dot");
+
+if (n > slides.length) {
+  slideIndex = 1;
+}
+
+if (n < 1) {
+  slideIndex = slides.length;
+}
+
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" explore-active", "");
+}
+
+slides[slideIndex - 1].style.display = "block";
+dots[slideIndex - 1].className += " explore-active";
+}
+
+// Function to navigate to the next slide
+function plusSlides(n) {
+showSlides((slideIndex += n));
+}
+
+// Function to set the current slide
+function currentSlide(n) {
+showSlides((slideIndex = n));
+}
+
+// Function to automatically advance slides
+function autoSlide() {
+plusSlides(1);
+setTimeout(autoSlide, 2000); // Adjust the time (in milliseconds) between slides
+}
+
+// Initial slide display and start automatic slideshow
+showSlides(slideIndex);
+autoSlide();
---
+++ script.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//mobile-friendly
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};  
+
+
+
